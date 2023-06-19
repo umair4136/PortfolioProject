57e6570387206ed36616f41bedda9b6a09208bbd
umair4136 <137055274+umair4136@users.noreply.github.com>
2023-06-19 14:50:12 +0500

    Update POrtfolioProject1.2.sql

--- POrtfolioProject1.2.sql
+++ POrtfolioProject1.2.sql
@@ -1,9 +1,9 @@
---select dea.location,dea.continent,dea.population,dea.date,vac.new_vaccinations 
---from CovidDeaths dea
---join Covidvaccinations vac
---on dea.location = vac.location 
---and dea.date = vac.date
---where dea.continent is not null and dea.continent like '%europe%' 
+select dea.location,dea.continent,dea.population,dea.date,vac.new_vaccinations 
+from CovidDeaths dea
+join Covidvaccinations vac
+on dea.location = vac.location 
+and dea.date = vac.date
+where dea.continent is not null and dea.continent like '%europe%' 
 
 select dea.location,dea.continent,dea.population,dea.date,vac.new_vaccinations,
 sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.location,dea.date) as PeopleVaccinated
