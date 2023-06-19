3925689c36dbe17d1d96158a04f7c6f4ec7b7798
umair4136 <137055274+umair4136@users.noreply.github.com>
2023-06-19 14:48:09 +0500

    Update PortfolioProject 1.1.sql

--- PortfolioProject 1.1.sql
+++ PortfolioProject 1.1.sql
@@ -1,49 +1,49 @@
---select *
---from CovidVaccinations
---order by 3,4
+select *
+from CovidVaccinations
+order by 3,4
 
---select * from coviddeaths
-
-
-
---select location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 as DeathPercentage
---from coviddeaths
---where location like 'pakistan'
---order by 1,2
-
---select location,date,total_cases,population,(total_cases/population)*100 as CasesPercentage
---from coviddeaths
---where location like 'pakistan'
---order by 1,2
-
---select Location, max(cast(total_cases as int)as HighInfectiousCount, Population,max(total_cases/population)*100) as PercentagePopulationInfected
---from coviddeaths
---group by population,location
---order by 4 desc
-
---select Location,Population,Max(cast(total_deaths as int))as TotalDeathCount,max((total_deaths/population)*100) as PercentagePeopleDiedPerPopulation
---from coviddeaths
---where continent is not null
---group by population,location
---order by 3 desc
+select * from coviddeaths
 
 
---select continent,Max(cast(total_deaths as int))as TotalDeathCount
---from coviddeaths
---where continent is not null
---group by continent
---order by 2 desc
 
---select location,Max(cast(total_deaths as int))as TotalDeathCount
---from coviddeaths
---where continent is null
---group by location
---order by TotalDeathCount desc
+select location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 as DeathPercentage
+from coviddeaths
+where location like 'pakistan'
+order by 1,2
 
---select date,sum(new_cases) as totalCases,sum(cast(new_deaths as int)) as TotalDeaths,sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPerc
---from CovidDeaths
---where continent is not null
---group by date
+select location,date,total_cases,population,(total_cases/population)*100 as CasesPercentage
+from coviddeaths
+where location like 'pakistan'
+order by 1,2
+
+select Location, max(cast(total_cases as int)as HighInfectiousCount, Population,max(total_cases/population)*100) as PercentagePopulationInfected
+from coviddeaths
+group by population,location
+order by 4 desc
+
+select Location,Population,Max(cast(total_deaths as int))as TotalDeathCount,max((total_deaths/population)*100) as PercentagePeopleDiedPerPopulation
+from coviddeaths
+where continent is not null
+group by population,location
+order by 3 desc
+
+
+select continent,Max(cast(total_deaths as int))as TotalDeathCount
+from coviddeaths
+where continent is not null
+group by continent
+order by 2 desc
+
+select location,Max(cast(total_deaths as int))as TotalDeathCount
+from coviddeaths
+where continent is null
+group by location
+order by TotalDeathCount desc
+
+select date,sum(new_cases) as totalCases,sum(cast(new_deaths as int)) as TotalDeaths,sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPerc
+from CovidDeaths
+where continent is not null
+group by date
 
 select sum(new_cases) as totalCases,sum(cast(new_deaths as int)) as TotalDeaths,sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPerc
 from CovidDeaths
